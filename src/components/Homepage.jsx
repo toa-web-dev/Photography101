4633fdb73e28c06086ec440756747bb1078cc297
gimHyoungSoo <gimhyoungsoo@gmail.com>
2023-12-03 14:27:49 +0900

    🏗️ jsx 구조 변경 - main.js에서 Homepage를 불러오며, Homepage에서 GlobalHeader의 nav로 다른 페이지로 전환함

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,16 @@
+import { GlobalHeader } from "./GlobalHeader.jsx";
+import "./Homepage.css";
+
+export function Homepage() {
+    return (
+        <div>
+            <GlobalHeader></GlobalHeader>
+            <section>
+              1번 섹션
+            </section>
+            <section>
+              2번 섹션
+            </section>
+        </div>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { Homepage } from "./components/Homepage";
+import "./reset.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <>
-        <App />
+        <Homepage></Homepage>
     </>
 );
