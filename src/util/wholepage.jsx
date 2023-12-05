021b0177746c90301ec0806e908f9aafab4da335
gimHyoungSoo <gimhyoungsoo@gmail.com>
2023-12-05 20:11:50 +0900

    ✨ 모든 페이지를 내보내는 util 파일 생성

---
+++ src/util/wholepage.jsx
@@ -0,0 +1,5 @@
+import { Homepage } from "../page/Homepage";
+import { Termpage } from "../page/Termpage";
+import { Advicepage } from "../page/Advicepage";
+
+export { Homepage, Termpage, Advicepage };
